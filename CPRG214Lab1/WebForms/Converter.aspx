270e471bc5ee959e3dc6f785ea92fb75ba1f3559
William Alfonso <wwallas@gmail.com>
2019-11-15 12:54:33 -0700

    application convert c#

---
+++ CPRG214Lab1/WebForms/Converter.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Converter.aspx.cs" Inherits="WebForms_Converter" UnobtrusiveValidationMode="None" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="/css/StyleSheet.css"/>
+</head>
+
+<body>
+    <form id="form1" runat="server">
+        <header>
+            <div>
+                <h2>Temperatures Converter:</h2>
+            </div>
+        </header>
+
+        <div class="column-layout">
+            <div class="call-out pimg1">
+                <%--<img src="smiley.gif" alt="Smiley face" height="42" width="42">--%>
+                <img src="../img/thermo.jpg" />
+            </div> 
+
+             <div class="call-out">
+                 <h3>Select of list temperatures to convert</h3>
+                 From     
+                 <asp:DropDownList ID="FromTmp" runat="server">
+                <asp:ListItem Selected="True" Value="1">Celsius</asp:ListItem>
+                <asp:ListItem Value="2">Fahrenheit</asp:ListItem>
+                <asp:ListItem Value="3">Kelvin</asp:ListItem>
+            </asp:DropDownList>
+                 To
+                 <asp:DropDownList ID="ToTmp" runat="server">
+                <asp:ListItem Value="1">Celsius</asp:ListItem>
+                <asp:ListItem Selected="True" Value="2">Fahrenheit</asp:ListItem>
+                <asp:ListItem Value="3">Kelvin</asp:ListItem>
+            </asp:DropDownList>
+
+                 <p>Type a value in the field to convert between temperature</p>
+                 <asp:TextBox ID="TxtValTmp" runat="server" ToolTip="Enter value" ValidateRequestMode="Enabled" MaxLength="5" CausesValidation="True">0</asp:TextBox>
+                 <br />
+                 <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="TxtValTmp" Display="Dynamic" ErrorMessage="Require a value to convert" ForeColor="Red" SetFocusOnError="True">Type a value to convert</asp:RequiredFieldValidator>
+                 <asp:RangeValidator ID="RangeValidator1" runat="server" ControlToValidate="TxtValTmp" Display="Dynamic" ErrorMessage="This field is a range between -1000 and 1000 grades" ForeColor="Red" MaximumValue="1000" MinimumValue="-1000" SetFocusOnError="True" Type="Double"></asp:RangeValidator>
+            <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ControlToValidate="TxtValTmp" ErrorMessage="It just acept numbers" ForeColor="Red" SetFocusOnError="True" ValidationExpression="^[-+]?\d*$"></asp:RegularExpressionValidator>
+            
+                 <div>
+                        <asp:Button ID="BtnConvert" runat="server" Text="Convert" />
+                        <asp:Button ID="BtnClear" runat="server" Text="Clear" OnClick="BtnClear_Click" />
+                 </div>
+            </div>
+
+             <div class="call-out">
+                 
+                 <h3>Output Temperature</h3>
+                 
+                 <p class="size">
+                    <asp:Label ID="LblResult" runat="server" BorderColor="#FFFF66"></asp:Label>
+                 </p>
+             
+             </div>
+
+        </div>  
+       
+    </form>
+</body>
+</html>
---
+++ CPRG214Lab1/WebForms/Converter.aspx.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+public partial class WebForms_Converter : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        decimal VlrCalculate = decimal.Parse(TxtValTmp.Text);
+        // Calculate the dropdownlist when is equal value
+        if (FromTmp.SelectedValue == ToTmp.SelectedValue)
+        {
+            LblResult.Text = TxtValTmp.Text;
+        }
+        
+        if (FromTmp.SelectedValue == "1" && ToTmp.SelectedValue == "2")
+        {
+            LblResult.Text = CalculateCenToFaren(VlrCalculate).ToString();
+        }
+
+        if (FromTmp.SelectedValue == "1" && ToTmp.SelectedValue == "3")
+        {
+            LblResult.Text = CalculateCenToKel(VlrCalculate).ToString();
+        }
+
+        if (FromTmp.SelectedValue == "2" && ToTmp.SelectedValue == "1")
+        {
+            LblResult.Text = CalculateFarenToCen(VlrCalculate).ToString();
+        }
+
+        if (FromTmp.SelectedValue == "2" && ToTmp.SelectedValue == "3")
+        {
+            LblResult.Text = CalculateFarenToKel(VlrCalculate).ToString();
+        }
+
+        if (FromTmp.SelectedValue == "3" && ToTmp.SelectedValue == "1")
+        {
+            LblResult.Text = CalculateKelToCen(VlrCalculate).ToString();
+        }
+
+        if (FromTmp.SelectedValue == "3" && ToTmp.SelectedValue == "2")
+        {
+            LblResult.Text = CalculateKelToFaren(VlrCalculate).ToString();
+        }
+
+
+    }
+
+    protected void BtnClear_Click(object sender, EventArgs e)
+    {
+        TxtValTmp.Text = "0";
+        FromTmp.SelectedValue = "1";
+        ToTmp.SelectedValue = "2";
+        LblResult.Text = " ";
+
+    }
+
+    public decimal CalculateCenToFaren(decimal VlrCalculate)
+    {
+        //℉=(℃*1.8)+32 
+
+        decimal total;
+        // check that the parameters are valid
+        total = (VlrCalculate * (decimal)1.8) + 32;
+
+        return Math.Round(total, 2);
+
+    }
+
+    public decimal CalculateCenToKel(decimal VlrCalculate)
+    {
+        //K=℃+273.15  
+
+        decimal total;
+        // check that the parameters are valid
+        total = VlrCalculate + (decimal)273.15;
+
+        return Math.Round(total, 2);
+
+    }
+
+    public decimal CalculateFarenToCen(decimal VlrCalculate)
+    {
+        // ℃=(℉-32)/1.8  
+
+        decimal total= 0.2m;
+        // check that the parameters are valid
+        total = (VlrCalculate - 32)/ (decimal)1.8;
+
+        return Math.Round(total, 2);
+
+    }
+
+    public decimal CalculateFarenToKel(decimal VlrCalculate)
+    {
+        //K=((℉-32)/1.8)+273.15
+        
+        decimal total;
+        // check that the parameters are valid
+        total = ((VlrCalculate -32) / (decimal)1.8) + (decimal)273.15;
+
+        return Math.Round(total, 2);
+
+    }
+
+    public decimal CalculateKelToCen(decimal VlrCalculate)
+    {
+        //℃=K-273.15 
+        decimal total;
+        // check that the parameters are valid
+        total = VlrCalculate - (decimal)273.15;
+
+        return Math.Round(total, 2);
+
+    }
+
+    public decimal CalculateKelToFaren(decimal VlrCalculate)
+    {
+        //℉=((K-273.15)*1.8)+32  
+        decimal total;
+        // check that the parameters are valid
+        total = ((VlrCalculate - (decimal)273.15)* (decimal)1.8) + 32;
+
+        return Math.Round(total, 2);
+
+    }
+}
